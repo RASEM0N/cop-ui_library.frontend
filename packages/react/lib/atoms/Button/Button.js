eb24022a271b4ea61d3dbba8a333b1a7a430c899
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-09-03 18:39:34 +0500

    add rollup to compile react

---
+++ packages/react/lib/atoms/Button/Button.js
@@ -0,0 +1,9 @@
+import { jsx } from 'react/jsx-runtime';
+import 'react';
+
+const Button = ({ label }) => {
+    return jsx("button", { children: label }, void 0);
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
---
+++ packages/react/rollup.config.js
@@ -0,0 +1,13 @@
+import Ts from 'rollup-plugin-typescript'
+
+export default {
+    input: ['src/index.ts', 'src/atoms/Button/index.ts'],
+    output: {
+        dir: 'lib',
+        format: 'esm',
+        sourcemap: true,
+    },
+    plugins: [Ts()],
+    preserveModules: true,
+    external: ['react']
+}
